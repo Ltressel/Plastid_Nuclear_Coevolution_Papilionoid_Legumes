2250d0e42f745886af825929180fd7d37e0949ec
Lydia Tressel <73486625+Ltressel@users.noreply.github.com>
2024-09-24 11:39:29 -0500

    Create run_trinity.sh

---
+++ Dataset_Assembly/run_trinity.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# run_trinity.sh
+# Assemble transcriptomes from non-rRNA reads using Trinity
+
+# Create output directory if it doesn't exist
+mkdir -p trinity_output
+
+# Run Trinity with paired-end non-rRNA reads
+Trinity --seqType fq --left non_rRNA_reads/*_R1_*.fastq.gz \
+        --right non_rRNA_reads/*_R2_*.fastq.gz \
+        --max_memory 50G --CPU 16 --output trinity_output/
+
+# Output message when completed
+echo "Transcriptome assembly complete. Results saved in trinity_output/"
